4080485da7e8a8295f75e95fec36e97950360ebd
dzky66 <andiidonk97@gmail.com>
2025-06-13 00:42:32 +0800

    perbaikan

--- frontend/src/pages/KontrolCepat.jsx
+++ frontend/src/pages/KontrolCepat.jsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { deviceAPI } from "../utils/api"
 import "../styles/KontrolCepat.css"
 import {
   FiZap,
